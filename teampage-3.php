05fd3559a0885a36001b56e3854bcb8e124fead0
mercyinyang <mercinyang@gmail.com>
2019-11-10 12:15:47 +0100

    Google analytics

--- teampage-3.php
+++ teampage-3.php
@@ -4,6 +4,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-151906986-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-151906986-1');
+</script>
 	<meta charset="UTF-8">
     <link rel="icon" type="image/png" href="https://res.cloudinary.com/thecavemann/image/upload/v1571839870/logo_g4kuoa.png"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
